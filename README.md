d807444812bc8888c81a9ad960ae1ee2177782ce
Tristan Le Guern <tleguern@bouledef.eu>
2015-04-04 10:41:06 +0200

    First import

---
+++ README.md
@@ -0,0 +1,16 @@
+libglarray.sh
+=============
+
+This repository contains an interface to uniformly declare global arrays
+in shell.
+
+Compatibility
+-------------
+
+libglarray.sh supports the following shells:
+
+- GNU Bourne-Again SHell - bash ;
+- MirBSD Korn SHell - mksh;
+- OpenBSD Korn SHell - oksh ;
+- Public Domain Korn SHell - pdksh;
+- The Z shell - zsh.
---
+++ glarray.sh
@@ -0,0 +1,65 @@
+#
+# Copyright (c) 2015 Tristan Le Guern <tleguern@bouledef.eu>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+LIBNAME="libglarray.sh"
+LIBVERSION="1.0"
+
+_glarrayksh() {
+	local _glarray_name="$1"
+	shift
+	set -A $_glarray_name -- $@
+}
+
+_glarraybash() {
+	local _glarray_name="$1"
+	shift
+	declare -ga $_glarray_name
+	local _glarray_i=0
+	local _glarray_j=0
+	for _glarray_i in $@; do
+		declare -ga "$_glarray_name[$_glarray_j]=$_glarray_i"
+		_glarray_j=$(( $_glarray_j + 1 ))
+	done
+}
+
+_glarrayzsh() {
+	local _glarray_name="$1"
+	shift
+	typeset -ga $_glarray_name
+	local _glarray_i=0
+	local _glarray_j=0
+	for _glarray_i in $@; do
+		typeset -g "$_glarray_name[$_glarray_j]"="$_glarray_i"
+		_glarray_j=$(( $_glarray_j + 1 ))
+	done
+}
+
+glarray_init() {
+	set +u
+	if [ -n "$BASH_VERSION" ]; then
+		glarray=_glarraybash
+	elif [ -n "$KSH_VERSION" ]; then
+		glarray=_glarrayksh
+	elif [ -n "$ZSH_VERSION" ]; then
+		setopt ksharrays
+		glarray=_glarrayzsh
+	else
+		echo "Error: unsuported shell :(" >&2
+		exit 1
+	fi
+	set -u
+}
+
